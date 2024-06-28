8947c5b1b2ae9361d8e10892b25526f9c221fa00
prem14-git <premkulkarni1407@gmail.com>
2024-06-29 00:32:08 +0530

    Create sum_of_n.cpp
    Time complexity is = O(2n)

---
+++ sum_of_n.cpp
@@ -0,0 +1,25 @@
+# include <bits/stdc++.h>
+using namespace std;
+int sumofN (int n)
+{
+	int sum=0;
+	if (n>0){
+		for (int i=1;i<=n;i++){
+		sum+=i;
+	}
+	}
+	else if (n<0) {
+		for (int i=n;i<=0;i++){
+			sum+=i;
+		}
+	}
+	return sum;
+}
+int main ()
+{
+	int n;
+	cout<<"Enter a number: ";
+	cin>>n;
+	cout<<"Sum of all numbers till "<<n<<" is "<<sumofN(n);
+	return 0;
+}
